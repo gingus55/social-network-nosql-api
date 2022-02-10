51e07822e681cecbe76ab122e9f26f5252e56be6
gingus55 <gingus55@hotmail.com>
2022-02-10 12:10:30 +0000

    more thoughts and reaction data removed

--- src/seeds/data/users.js
+++ src/seeds/data/users.js
@@ -3,26 +3,31 @@ const users = [
     username: "bobsmith",
     email: "bobsmith@email.com",
     thoughts: [],
+    friends: [],
   },
   {
     username: "tomsmith",
     email: "tomsmith@email.com",
     thoughts: [],
+    friends: [],
   },
   {
-    username: "bobbydoo",
+    username: "grogu",
     email: "bobbydoo@email.com",
     thoughts: [],
+    friends: [],
   },
   {
     username: "charlie",
     email: "charlie@email.com",
     thoughts: [],
+    friends: [],
   },
   {
     username: "mando",
     email: "mando@email.com",
     thoughts: [],
+    friends: [],
   },
 ];
 
