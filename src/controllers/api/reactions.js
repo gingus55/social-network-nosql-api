d12df9417e0e444c2c763e9d1aa265f4e76de265
gingus55 <gingus55@hotmail.com>
2022-02-12 17:57:43 +0000

    create reaction for a thought

--- src/controllers/api/reactions.js
+++ src/controllers/api/reactions.js
@@ -1,5 +1,24 @@
-const addReactionForThought = (req, res) => {
-  res.send("addReactionForThought");
+const { Thought } = require("../../models");
+
+const addReactionForThought = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { reactionBody, username } = req.body;
+    const thought = await Thought.findById(id);
+
+    thought.reactions.push({ reactionBody, username });
+
+    await Thought.replaceOne({ _id: id }, thought);
+
+    const updatedThought = await Thought.findById(id);
+
+    return res.json({ success: true, data: updatedThought });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create thought | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create thought" });
+  }
 };
 const deleteReactionOfThought = (req, res) => {
   res.send("deleteReactionOfThought");
